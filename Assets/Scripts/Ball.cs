4894b33a387f42bef4f34fe11b1cc0900813cfaa
Vanessa Chantal <vanesita1208@gmail.com>
2026-04-15 13:08:40 -0600

    proceso de programa de pelota

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,7 +6,9 @@ public class Ball : MonoBehaviour
 {
     public new Rigidbody2D rigidbody { get; private set; }
 
-    public float speed = 500f;
+    public float speed = 30f;
+
+    public float maxBounceAngle = 75f;
 
     private void Awake() {
         this.rigidbody = GetComponent<Rigidbody2D>();
@@ -22,5 +24,23 @@ public class Ball : MonoBehaviour
         force.y = -1f;
 
         this.rigidbody.AddForce(force.normalized * this.speed);
+    }
+
+    private void CollisionEnter2D(Collision2D collision) {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+
+        if (ball != null)
+        {
+            Vector3 paddlePosition = this.transform.position;
+            Vector2 contactPoin =  collision.GetContact(0).point;
+
+            float offset = paddlePosition.x - contactPoin.x;
+            float width = collision.otherCollider.bounds.size.x / 2;
+
+            float currentAngle = Vector2.SignedAngle(Vector2.up, ball.rigidbody.velocity);
+            float bounceAngle = (offset /  width) * this.maxBounceAngle;
+            float newAngle = Mathf.Clamp (currentAngle + bounceAngle, -this.maxBounceAngle, this.maxBounceAngle);
+            this.rigidbody.AddForce(Vector2.up * this.speed * newAngle);
+        }
     }
 }
